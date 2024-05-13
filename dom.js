9091187c925e629ec3e3039c31df253b1b4fabd4
Karthick R <53260064+karthick0529@users.noreply.github.com>
2024-05-13 20:35:53 +0530

    dom js file

---
+++ dom.js
@@ -0,0 +1,11 @@
+function foo(){
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var email = document.getElementById("mail").value;
+    var location = document.getElementById("location").value;    
+    
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Location: " + location);
+}
